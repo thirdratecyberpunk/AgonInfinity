6a614f1a3d763fcca31c3cb4a1513d58adb995cc
Lewis Blackburn <blackbul@aston.ac.uk>
2017-02-27 19:33:23 +0000

    Changed node shapes to fully coloured circles.

--- album.js
+++ album.js
@@ -8,25 +8,26 @@ function draw() {
     // 9 used as default parameter because Nonagon
     var nodeNums = 9;
 
-    // need to calculate the specific location of each node
-    // howevermanyradiansinacircle/nodeNums should provide a number of radians to put nodes at
+    // calculates the radians for the arc to populate the circle with
     var radians = 6.283/nodeNums;
     var startAngle = 0;
     var endAngle = 0;
     var nodeLocation = [];
+    // defines colours for fill style
+    ctx.fillStyle = 'rgb(365, 0, 0)';
     // renders a node nodeNums times
     for (i = 0; i < nodeNums; i++){
-      // defines colours for fill style
-      ctx.fillStyle = 'rgb(365, 0, 0)';
       ctx.beginPath();
       endAngle = startAngle + radians;
       console.log(endAngle);
+      // creates the arc used to define the centre of the node
       ctx.arc(250, 250, 125, startAngle, endAngle);
       ctx.stroke();
+      // gets the end of the arc
       nodeLocation = getPoint(250, 250, 125, endAngle);
-      console.log(nodeLocation);
-      // creates a square with co-ordinates of the given points
-      ctx.fillRect(nodeLocation[0], nodeLocation[1], 10, 10);
+      // creates a circle centred at the end of the arc
+      ctx.arc(nodeLocation[0],nodeLocation[1],10,0,2*Math.PI);
+      ctx.fill();
       startAngle = endAngle;
     }
   }
