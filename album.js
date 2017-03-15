29d70e9ca47ac914f6f12aaaf9bbc566ebcd9a2a
Lewis Blackburn <blackbul@aston.ac.uk>
2017-03-15 10:25:14 +0000

    Added implementation of Node as a class.

--- album.js
+++ album.js
@@ -1,3 +1,15 @@
+// class representing a 'node' point
+function Node(endPointX, endPointY){
+  this.endPointX = endPointX;
+  this.endPointY = endPointY;
+  this.getNodeX= function(){
+    return this.endPointX;
+  }
+  this.getNodeY= function(){
+    return this.endPointY;
+  }
+}
+
 // responsible for rendering the album art itself
 function draw() {
   var canvas = document.getElementById('canvas');
@@ -15,21 +27,23 @@ function draw() {
     var nodeLocation = [];
     // defines colours for fill style
     ctx.fillStyle = 'rgb(365, 0, 0)';
-    // renders a node nodeNums times
+    // renders nodes nodeNums times
+    var nodes = {};
     for (i = 0; i < nodeNums; i++){
       ctx.beginPath();
       endAngle = startAngle + radians;
-      console.log(endAngle);
       // creates the arc used to define the centre of the node
       ctx.arc(250, 250, 125, startAngle, endAngle);
       ctx.stroke();
       // gets the end of the arc
       nodeLocation = getPoint(250, 250, 125, endAngle);
+      var node = new Node(nodeLocation[0], nodeLocation[1]);
       // creates a circle centred at the end of the arc
-      ctx.arc(nodeLocation[0],nodeLocation[1],10,0,2*Math.PI);
-      ctx.fill();
+      ctx.arc(node.getNodeX(),node.getNodeY(),5,0,2*Math.PI);
+      ctx.stroke();
       startAngle = endAngle;
     }
+    // draws lines from every node to every other node
   }
 }
 
